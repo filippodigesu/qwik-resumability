49406e2c0b2d11435a7770206a95332cf14dd588
filippodigesu <filippo.diges2@gmail.com>
2023-05-21 15:10:23 +0200

    final

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -24,7 +24,7 @@ export default component$(() => {
                   <div class="flex w-full sm:w-auto">
                     <a
                       class="btn btn-primary sm:mb-0 w-full"
-                      href="https://pagespeed.web.dev/analysis/https-qwik-resumability-vercel-app/52s5buu969?form_factor=desktop"
+                      href="https://pagespeed.web.dev/analysis/https-qwik-resumability-vercel-app/o4w8v55dhs?form_factor=mobile"
                       target="_blank"
                       rel="noopener"
                     >
